4d9de478947a4a568c79281d76257e828ef5fa26
sssrank <kingjieyou@qq.com>
2016-03-08 00:47:55 +0800

    elk

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t elk -f elk.dockfile .
--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
-docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm -f
-docker images|grep none|awk '{print $3 }'|xargs docker rmi
+docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm -fv
+docker images|grep none|awk '{print $3 }'|xargs docker rmi 
